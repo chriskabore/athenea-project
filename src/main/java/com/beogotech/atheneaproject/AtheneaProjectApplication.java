1bccddf3af9df499086afbae4eb64a35270ab2ee
Christian Kabore <chriskabor@gmail.com>
2020-05-21 12:06:09 +0000

    added graddle settings and source files

---
+++ src/main/java/com/beogotech/atheneaproject/AtheneaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.beogotech.atheneaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtheneaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtheneaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beogotech/atheneaproject/AtheneaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beogotech.atheneaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtheneaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

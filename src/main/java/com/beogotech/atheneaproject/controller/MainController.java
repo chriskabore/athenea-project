5ff7c730093c8a83d41afb8e18eade725fce5179
Christian Kabore <chriskabor@gmail.com>
2020-08-25 13:42:12 +0000

    edited dashboard.html to modify colors on sythesis block

--- src/main/java/com/beogotech/atheneaproject/controller/MainController.java
+++ src/main/java/com/beogotech/atheneaproject/controller/MainController.java
@@ -5,30 +5,79 @@ import com.beogotech.atheneaproject.utils.LoggerFactoryUtil;
 import com.beogotech.atheneaproject.utils.TreeNode;
 import com.google.gson.Gson;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
 	
 	private static Logger LOG = LoggerFactoryUtil.getLog(MainController.class);
+	
+	
+	private MessageSource messageSource;
+	private MessageSourceAccessor accessor;
+	
+	@Autowired
+	public MainController(MessageSource messageSource){
+		this.messageSource = messageSource;
+		this.accessor  = new MessageSourceAccessor(this.messageSource, LocaleContextHolder.getLocale());
+	}
+	
+	
 
 	@GetMapping(value = "/dashboard")
 	public String showIndexPage(Model model){
+		int rateOfBudgetSpent = 52;
+		String budgetChart = buildBudgetChart(rateOfBudgetSpent);
 		model.addAttribute("year", AtheneaUtil.COPYRIGHT_YEAR_INT);
 		model.addAttribute("numberOfNotifications", "3");
 		model.addAttribute("numberOfMessages", "2");
+		model.addAttribute("totalOfBudget", "35 000 000 000 FCFA");
+		model.addAttribute("budgetAvailableValue", "16 800 000 000 FCFA");
+		model.addAttribute("budgetSpentValue", "18 200 000 000 FCFA");
+		model.addAttribute("rateOfBudgetSpent", rateOfBudgetSpent);
+		model.addAttribute("financialImplementationRate", 0.6);
+		model.addAttribute("budgetSpentChart", budgetChart);
 		model.addAttribute("actionPlanActiveOption", "PA1 - PLAN D'ACTION BUDGETISE DU PAAQE 2018-2020");
 		model.addAttribute("actionPlanComponents",new String[] {"CMP001 - Elargissement de l'accès equitable...", "CMP002 - Amelioration de la qualité du processus...", "CMP003 - Renforcement de la capacité institutionnelle..."});
 		return "/dashboard";
+	}
+	
+	private String buildBudgetChart(int rateOfBudgetSpent) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<svg viewBox=\"0 0 40 40\" class=\"circular-chart green\">");
+		sb.append("\n");
+		sb.append("<path class=\"circle-bg\"\n" +
+				"                                                                              d=\"M18 2.0845\n" +
+				"                                                                      a 15.9155 15.9155 0 0 1 0 31.831\n" +
+				"                                                                      a 15.9155 15.9155 0 0 1 0 -31.831\"\n" +
+				"                                                                        />");
+		sb.append("<path class=\"circle\"");
+		sb.append("stroke-dasharray=\""+rateOfBudgetSpent+","+"100"+"\"");
+		
+		sb.append("d=\"M18 2.0845\n" +
+				"                                                                      a 15.9155 15.9155 0 0 1 0 31.831\n" +
+				"                                                                      a 15.9155 15.9155 0 0 1 0 -31.831\"\n" +
+				"                                                                        />");
+		sb.append("<text x=\"18\" y=\"16\" class=\"percentage\"");
+		sb.append("th:text=\""+rateOfBudgetSpent+"%"+"\"");
+		sb.append(">");
+		sb.append(rateOfBudgetSpent+"%");
+		sb.append("</text>");
+		sb.append("<text x=\"18\" y=\"24\" class=\"percentage\">");
+		sb.append(accessor.getMessage("ui.dashboard.content.spent", LocaleContextHolder.getLocale()));
+		sb.append("</text>");
+		sb.append("</svg>");
+		return sb.toString();
 	}
 	
 	@GetMapping(value = {"/", "/login"})
